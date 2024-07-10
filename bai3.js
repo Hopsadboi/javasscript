ec4de3fd34cabb8cf0a7fa7a2a890503a96967ff
Hopsadboi <hoahop7a1@gmail.com>
2024-07-10 16:57:56 +0700

    Revert "Bai3"

--- bai3.js
+++
@@ -1,7 +0,0 @@
-console.log("con cu")
-let x= 1, y=2 , z=3;
-console.log("test string", 'x=', x, 'y=', y,'z=', z)
-// let khai báo biến được cả số nguyên, số thực
-// mẹo dùng dấu (`) này bọc ngoài, sài duoc bên trong là (",')thoải mái
-// string quan trọng
-// dấu + cộng các thứ lại với nhau
