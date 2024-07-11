9c1d635096afb4a3cb4ea8e02621df7a3697c17c
Hopsadboi <hoahop7a1@gmail.com>
2024-07-11 19:52:02 +0700

    bai9sosanh

---
+++ hocJS/bai9.js
@@ -0,0 +1,15 @@
+console.log('hello wrold from html');
+let a= 5;
+let b= 5;
+console.log('compare a,b:','a=', a, 'b=', a, 'result:', a===b) 
+// nên dùng 3 dấu = (===) để so sánh
+// === so sánh type (kiểu:number,strings), value (giá trị)
+// == chỉ so sánh value 
+
+// Empty khi không đặc giá trị cho nó
+let c= {} //vd là một Empty
+console.log('test value:',c)
+
+let d ;  //vd là một undefined
+console.log('test value:', d)
+// undefined là đã tạo biến nhưng chưa gắn giá trị cho nó
