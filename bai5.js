89d350d5358964f24a9c13a94415a22186aee707
Hopsadboi <hoahop7a1@gmail.com>
2024-07-11 11:15:08 +0700

    bai5 number

---
+++ bai5.js
@@ -0,0 +1,11 @@
+console.log('hello wrold from HTML')
+let a= 5;
+let b= 6;
+let c= 5.5;
+let d= '5'
+let e= +d
+let text= 'abcdefk'
+console.log('e value:', e, 'type:',typeof e)
+console.log(a+c)
+console.log('c value:', c, 'type:',typeof c)
+console.log('text value:', text,'type:',typeof text)
