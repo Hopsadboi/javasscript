cdc4eced551bc2f097b8d37361066540893c0021
Hopsadboi <hoahop7a1@gmail.com>
2024-07-11 21:15:22 +0700

    bai10for

---
+++ hocJS/bai10.js
@@ -0,0 +1,10 @@
+console.log('hello wrold from HTML')
+ // vòng lặp for
+ // chia thành 3 phần, mỗi phần ngăn cách nhau bởi dấu (;)
+ // phần 1: khởi tạo giá trị i
+ // phần 2: check điều kiện i <= 10
+ // phần 3: chạy dk vd i=1+i=1: kq i=2 chạy theo dị cho tới khi bằng 11 nhỏ hơn 10 
+ let arr = ['MU', 'Mancity', 'Liver', 'CL']
+ for (let i = 0; i < arr.length; i++) {
+  console.log('check variable i = ', arr[i])
+ } 
