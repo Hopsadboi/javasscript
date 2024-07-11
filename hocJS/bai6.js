ba001cbeb39aec38a3da93f653fe5ca311a92969
Hopsadboi <hoahop7a1@gmail.com>
2024-07-11 13:12:42 +0700

    bai7

--- hocJS/bai6.js
+++ hocJS/bai6.js
@@ -2,6 +2,6 @@ console.log('hello wrold from html')
 let a= 5;
 let b= 10;
 console.log(`hopdz check string: a= ${a} b= ${b} a+b= ${a+b}`)
-// sử dụng ($) và {} để khai báo biến nhanh hơn 
+// sử dụng ($) và {} để nối chuỗi dễ nhanh hơn
 console.log(`/hopdz/${a}`)
 // url: /hopdz/5
---
+++ hocJS/bai7.js
@@ -0,0 +1,15 @@
+console.log('hello wrold from html')
+// object nghĩa là ( đối tượng ) hành động từ con người,vật.. từ đời sống vào code
+// key (từ khóa): value ( giá trị)
+// object ngăn cách với nhau bởi dấu (,)
+let object= {
+    name: 'HOPDZ', 
+    address: 'AN GIANG',
+    email: '',
+    
+};
+// được hiểu là 1 object
+let b= 'HOPDZ';
+console.log(`what your name ?, my name is:`, object.name)
+console.log(`where are you from ?, i am from:`, object.address)
+// object sử dụng dấu (.) để chạy key
