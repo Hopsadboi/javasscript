6cd2e58600262470894d6ceb5b1dcb012fea1946
Hopsadboi <hoahop7a1@gmail.com>
2024-07-11 18:40:18 +0700

    bai8array

---
+++ hocJS/bai8.js
@@ -0,0 +1,11 @@
+//Array khai báo bằng dấu [] 
+//Array =[item1, item2, item3] bên trong lưu các phần tử 
+// Array dùng để lưu trữ  object bên trong
+console.log('hello wrold from HTML');
+let a= ['MU', 'liver','chelsea']
+console.log('typof a:',typeof a,a)
+let b= [
+    {name: 'HOPDZ',age: 13},
+  
+]
+console.log('name and age:',b)
