c13dd1124715f892d84be5e22b13bdb8fb480fad
Hopsadboi <hoahop7a1@gmail.com>
2024-07-12 15:44:02 +0700

    bai12function

---
+++ hocJS/bai12.js
@@ -0,0 +1,11 @@
+console.log('hello wrold from HTML')
+
+// function nghĩa là một hàm 
+// đặt tên cho hàm là một động từ
+// là input trả cho đầu ra vd sum= tính tổng
+function sum( a, b) {
+    return a + b;
+}
+let c = sum(10,20)
+console.log('sum c',c)
+console.log('sum a + b = ', sum(6,9))
